50468b8f3d2f148f438187a1f2f595bb85873e67
MrNwear <mr.y.neuer@gmail.com>
2024-05-18 16:27:31 +0300

    export all components

---
+++ src/Components/index.ts
@@ -0,0 +1,5 @@
+export {default as Loader} from './Loader';
+export {default as SepratorLine} from './SepratorLine';
+export {default as PostCard} from './PostCard';
+export {default as Avatar} from './Avatar';
+export {default as Comment} from './Comment';
